bfc553fa2351818ce870ab6a2502e0d3e6fa7cde
miu-kimita <miu.6ki26.bigdaddy@gmail.com>
2021-06-28 11:21:11 +0000

    マイグレーションファイル変更後

--- db/migrate/20210621083404_create_favorites.rb
+++ db/migrate/20210621083404_create_favorites.rb
@@ -1,6 +1,6 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
-    create_table :favorites do |t|
+    create_table :favorites, id: :integer do |t|
       t.integer :post_user_id
       t.integer :post_id
 
